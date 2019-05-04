92e0e5eb5e07a4a2b27980c74e199224e376867b
BrunoVAranha <bruno.va.github@gmail.com>
2019-05-04 18:28:32 -0300

    Criação da classe Tabuleiro

--- XadrezCsharp/Program.cs
+++ XadrezCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace XadrezCsharp
 {
@@ -6,6 +7,11 @@ namespace XadrezCsharp
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
+            Console.ReadLine();
         }
     }
 }
